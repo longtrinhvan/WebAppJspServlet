d3f6f31122d55051400645f403546c02d6554209
DELL <DELL@DESKTOP-1PIQDM5>
2020-06-23 21:03:25 +0700

    function cart

--- WebAppJspServlet/src/main/java/com/webapp/web/CartController.java
+++ WebAppJspServlet/src/main/java/com/webapp/web/CartController.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.webapp.model.userModel;
+import com.webapp.utils.SessionUtil;
+
 @WebServlet(urlPatterns = { "/gio-hang"})
 public class CartController extends HttpServlet {
 
@@ -16,7 +19,24 @@ public class CartController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+       
+		userModel model = (userModel) SessionUtil.getInstance().getValue(request, "usermodel");
+		String thongBao="";
+		if (model != null) {
+			if (model.getRoleid() == 2) {
+				thongBao="Tài khoản không hỗ trợ chức năng này!!!";
+				request.setAttribute("thongBao", thongBao);
+				RequestDispatcher rd = request.getRequestDispatcher("/views/web/login/login.jsp");
+				rd.forward(request, response);
+				return;
+			} 
+		} else {
+			thongBao="Vui lòng đăng nhập!!!";
+			request.setAttribute("thongBao", thongBao);
+			RequestDispatcher rd = request.getRequestDispatcher("/views/web/login/login.jsp");
+			rd.forward(request, response);
+			return;
+		}
 		RequestDispatcher rd = request.getRequestDispatcher("/views/web/cart/cart.jsp");
 		rd.forward(request, response);
 	}
